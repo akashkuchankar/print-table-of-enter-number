558194af1d3220865131bb3268c3011dcef3251d
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 07:54:39 -0700

    Add project files.

---
+++ print table of enter number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_table_of_enter_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  N, M;
+            Console.WriteLine("enter any number");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (M = 1; M <= 10; M++)
+            {
+                Console.WriteLine("{0}*{1}={2}",N,M,(N*M));
+                Console.ReadLine();
+            }
+        }
+    }
+}
